be738cd6daf9315f0ed2419133f4e564fac39223
Alexey Nosov <nayretech@gmail.com>
2022-08-26 08:29:45 +0700

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,7 +31,9 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vuetify/no-deprecated-classes': 'error',
     'vuetify/grid-unknown-attributes': 'error',
-    'vuetify/no-legacy-grid': 'error'
+    'vuetify/no-legacy-grid': 'error',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-mutating-props': 'off'
   },
   overrides: [
     {
